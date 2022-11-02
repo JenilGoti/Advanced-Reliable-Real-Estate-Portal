f973882e963741f9255fb628cea86f5d45436b25
jenil <gotijenil23@gmail.com>
2022-11-03 00:17:00 +0530

    commit at ${COMMIT_TIMESTAMP}

--- public/js/property/property.js
+++ public/js/property/property.js
@@ -1,6 +1,8 @@
 let slideIndex = 1;
 const host = location.protocol + '//' + location.host;
 const propertyId = document.getElementById("prop-id").value;
+const close = document.querySelector(".close")
+const isAuth = document.getElementById("is-auth").value;
 const myModel = document.getElementById("myModel");
 const favoritBtn = document.querySelector(".favorite");
 const commentBtn = document.querySelector(".comment");
@@ -16,11 +18,13 @@ const shareData = {
 console.log(propertyId);
 
 function openModal() {
+    close.style.display="unset";
     myModel.classList.remove("slideshow-container")
     myModel.classList.add("modal");
 }
 
 function closeModal() {
+    close.style.display="none";
     myModel.classList.remove("modal")
     myModel.classList.add("slideshow-container");
 }
@@ -56,74 +60,74 @@ function showSlides(n) {
 }
 
 
+if (isAuth == "true") {
+    // bookmark btn
 
-// bookmark btn
-
-fetch(host + "/property/bookmark/" + propertyId, {
-        method: 'GET'
-    })
-    .then(result => result.json())
-    .then(result => {
-        if (result.bookmark) {
-            bookmarkBtn.style.color = "#E8AA42";
-        }
-        bookmark = result.bookmark;
-
-    })
-    .catch(err => console.log(err));
-
-bookmarkBtn.addEventListener("click", () => {
-    console.log("bookmark " + propertyId);
     fetch(host + "/property/bookmark/" + propertyId, {
-            method: 'POST',
-            body: new URLSearchParams("bookmark=" + bookmark),
+            method: 'GET'
         })
         .then(result => result.json())
         .then(result => {
             if (result.bookmark) {
                 bookmarkBtn.style.color = "#E8AA42";
-            } else {
-                bookmarkBtn.style.color = "inherit";
             }
             bookmark = result.bookmark;
+
         })
         .catch(err => console.log(err));
-});
 
-// like btn
+    bookmarkBtn.addEventListener("click", () => {
+        console.log("bookmark " + propertyId);
+        fetch(host + "/property/bookmark/" + propertyId, {
+                method: 'POST',
+                body: new URLSearchParams("bookmark=" + bookmark),
+            })
+            .then(result => result.json())
+            .then(result => {
+                if (result.bookmark) {
+                    bookmarkBtn.style.color = "#E8AA42";
+                } else {
+                    bookmarkBtn.style.color = "inherit";
+                }
+                bookmark = result.bookmark;
+            })
+            .catch(err => console.log(err));
+    });
 
-fetch(host + "/property/like/" +propertyId, {
-        method: 'GET'
-    })
-    .then(result => result.json())
-    .then(result => {
-        if (result.like) {
-            favoritBtn.style.color = "#E8AA42";
-        }
-        like = result.like;
+    // like btn
 
-    })
-    .catch(err => console.log(err));
-
-
-favoritBtn.addEventListener("click", () => {
-    console.log("like " + propertyId);
-    fetch(host + "/property/like/" +propertyId, {
-            method: 'POST',
-            body: new URLSearchParams("like=" + like),
+    fetch(host + "/property/like/" + propertyId, {
+            method: 'GET'
         })
         .then(result => result.json())
         .then(result => {
             if (result.like) {
                 favoritBtn.style.color = "#E8AA42";
-            } else {
-                favoritBtn.style.color = "inherit";
             }
             like = result.like;
+
         })
         .catch(err => console.log(err));
-});
 
+
+    favoritBtn.addEventListener("click", () => {
+        console.log("like " + propertyId);
+        fetch(host + "/property/like/" + propertyId, {
+                method: 'POST',
+                body: new URLSearchParams("like=" + like),
+            })
+            .then(result => result.json())
+            .then(result => {
+                if (result.like) {
+                    favoritBtn.style.color = "#E8AA42";
+                } else {
+                    favoritBtn.style.color = "inherit";
+                }
+                like = result.like;
+            })
+            .catch(err => console.log(err));
+    });
+}
 // share btn
 
 shareBtn.addEventListener("click", () => {

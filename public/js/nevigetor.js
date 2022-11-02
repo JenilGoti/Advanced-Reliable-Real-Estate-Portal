6c2b09fc9a5850eada27d72227b2e0fba6a5f49c
jenil <gotijenil23@gmail.com>
2022-11-02 23:28:06 +0530

    commit at ${COMMIT_TIMESTAMP}

--- public/js/nevigetor.js
+++ public/js/nevigetor.js
@@ -1,9 +1,11 @@
+const nev = document.querySelector(".main-header");
 const nevbar = document.querySelector(".main-header__nav");
 const nevbarTitle = document.getElementsByClassName("manu-title");
 const floattingActionButton = document.querySelector(".floating-action-button");
 const nevbarBackground = document.querySelector(".nevbar-background");
 
 function mouseOver() {
+    nev.style.animation= "drover-display1 .3s";
     for (var i = 0; i < nevbarTitle.length; i++) {
         nevbarTitle[i].style.animation= "scale-display .3s";
         nevbarTitle[i].style.display = "table-cell";
@@ -12,17 +14,20 @@ function mouseOver() {
 }
 
 function mouseOut() {
+    nev.style.animation= "drover-display--reverse1 .3s"
     for (var i = 0; i < nevbarTitle.length; i++) {
         nevbarTitle[i].style.display = "none";
     }
 }
 
 function showNewBar() {
+    nev.style.animation= "drover-display .3s";
     nevbar.style.display = "flex";
     floattingActionButton.style.display = "none";
     nevbarBackground.style.display = "flex";
 }
 function hideNewBar() {
+    nev.style.animation= "drover-display--reverse .10s";
     nevbar.style.display = "none";
     floattingActionButton.style.display = "flex";
     floattingActionButton.style.animation= "scale-display .3s";
